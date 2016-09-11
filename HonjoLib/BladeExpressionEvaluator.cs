5dbb2678d001e779c37e7b7417d03f4bd8b0bd5a
contactsamie <contactsamie@gmail.com>
2016-09-11 15:19:31 -0400

    update

--- HonjoLib/BladeExpressionEvaluator.cs
+++ HonjoLib/BladeExpressionEvaluator.cs
@@ -1,20 +1,8 @@
-using DynamicExpresso;
 using Microsoft.JScript;
 using Microsoft.JScript.Vsa;
 
 namespace HonjoLib
 {
-    public class DynamicExpressoExpressionEvaluator : IBladeExpressionEvaluator
-    {
-        public string Evaluate(string expression)
-        {
-           
-            var interpreter = new Interpreter();
-            var result = interpreter.Eval(expression);
-
-            return result.ToString();
-        }
-    }
     public class BladeExpressionEvaluator : IBladeExpressionEvaluator
     {
         public string Evaluate(string expression)
---
+++ HonjoLib/DynamicExpressoExpressionEvaluator.cs
@@ -0,0 +1,16 @@
+using DynamicExpresso;
+
+namespace HonjoLib
+{
+    public class DynamicExpressoExpressionEvaluator : IBladeExpressionEvaluator
+    {
+        public string Evaluate(string expression)
+        {
+           
+            var interpreter = new Interpreter();
+            var result = interpreter.Eval(expression);
+
+            return result.ToString();
+        }
+    }
+}
